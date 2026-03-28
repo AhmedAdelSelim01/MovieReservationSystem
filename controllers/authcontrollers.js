624a0da8e5707713ccf62651a604862b9c75112d
Ahmed Selim <ahmedadelll364@gmail.com>
2026-03-28 16:04:36 +0200

    refactor: improve response structure in registerUser function

--- controllers/authcontrollers.js
+++ controllers/authcontrollers.js
@@ -11,7 +11,12 @@ export const registerUser = catchAsync(async (req, res, next) => {
   }
   // Create new user
   const user = await User.create({ name, email, password, role });
-  res.status(201).json({ message: "User registered successfully", user });
+  res
+    .status(201)
+    .json({
+      message: "User registered successfully",
+      user: { id: user._id, name: user.name, email: user.email },
+    });
 });
 
 export const loginUser = catchAsync(async (req, res, next) => {
