05345561e943560569b5c2c36faa6a2edd5967ef
Ahmed Selim <ahmedadelll364@gmail.com>
2026-03-30 00:47:17 +0200

    feat: add routes for retrieving, deleting, and editing showtimes by ID

--- router/showtimeRoutes.js
+++ router/showtimeRoutes.js
@@ -5,7 +5,10 @@ import { showtimeValidator } from "../validators/showtimeValidator.js";
 import { validate } from "../middlewares/validate.js";
 import {
   createShowtime,
+  deleteShowtime,
+  editShowtime,
   getAllShowtimes,
+  getShowtimeById,
 } from "../controllers/showtimeController.js";
 
 const router = express.Router();
@@ -14,5 +17,14 @@ const router = express.Router();
 router.post("/", protect, isAdmin, showtimeValidator, validate, createShowtime);
 // get all showtimes
 router.get("/", getAllShowtimes);
+
+//get a showtime by id
+router.get("/:id", protect, getShowtimeById);
+
+// detele ashowtime by id admin only
+router.delete("/:id", protect, isAdmin, deleteShowtime);
+
+// edit a showtime by id admin only
+router.put("/:id", protect, isAdmin, showtimeValidator, validate, editShowtime);
 
 export default router;
