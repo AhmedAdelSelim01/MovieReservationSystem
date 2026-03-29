18d9075ca565d6984d6f0f8a7e10dac1a9768c6d
Ahmed Selim <ahmedadelll364@gmail.com>
2026-03-30 00:47:07 +0200

    feat: add admin routes for retrieving all reservations and specific reservation by ID

--- router/reservationRoutes.js
+++ router/reservationRoutes.js
@@ -3,13 +3,29 @@ import { protect } from "../middlewares/auth.js";
 import { reservationValidator } from "../validators/reservationValidator.js";
 import { validate } from "../middlewares/validate.js";
 import {
+  cancelReservation,
   createReservation,
+  getAllReservations,
+  getReservationById,
   getUserReservations,
 } from "../controllers/reservationControllers.js";
+import isAdmin from "../middlewares/isAdmin.js";
 
 const router = Router();
 
+// Create a new reservation
 router.post("/", protect, reservationValidator, validate, createReservation);
+
+// Get all reservations for the logged-in user
 router.get("/", protect, getUserReservations);
+
+//this route is for admin to get all reservations
+router.get("/all", protect, isAdmin, getAllReservations);
+
+// delete a reservation by the user who made it or by an admin
+router.delete("/:id", protect, cancelReservation);
+
+// get a reservation by id for the user who made it or for an admin
+router.get("/:id", protect, getReservationById);
 
 export default router;
