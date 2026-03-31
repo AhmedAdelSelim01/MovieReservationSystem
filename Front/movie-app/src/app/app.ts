713ba6d71a5cf8248eb1581104fd171c8821ee87
Ahmed Selim <ahmedadelll364@gmail.com>
2026-04-01 00:54:15 +0200

    feat: add Navbar and Footer components to app structure

--- Front/movie-app/src/app/app.ts
+++ Front/movie-app/src/app/app.ts
@@ -2,11 +2,13 @@ import { Component, signal } from '@angular/core';
 import { Hero } from './components/hero/hero';
 import { NowShowing } from './components/now-showing/now-showing';
 import { Upcoming } from "./components/upcoming/upcoming";
+import { Footer } from "./components/footer/footer";
+import { Navbar } from "./components/navbar/navbar";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Hero, NowShowing, Upcoming],
+  imports: [Hero, NowShowing, Upcoming, Footer, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
