b461c80bb8e6b9a3e697774520d64fe0eb6ceb64
Ahmed Selim <ahmedadelll364@gmail.com>
2026-04-01 18:26:52 +0200

    feat: Refactor movie app components and routes
    - Removed Upcoming and Now Showing components and their associated tests, styles, and HTML files.
    - Created new Hero component with styling and HTML structure.
    - Introduced MovieCard component for displaying individual movie details.
    - Updated routing to reflect new component structure.
    - Added Footer and Navbar components with their respective styles and HTML.
    - Implemented NowShowing component to display currently showing movies.
    - Added Upcoming component to showcase upcoming movie releases.
    - Cleaned up unused styles and HTML files across the application.

--- Front/movie-app/src/app/features/movies/movies.routes.ts
+++ Front/movie-app/src/app/features/movies/movies.routes.ts
@@ -3,14 +3,16 @@ import { Routes } from '@angular/router';
 export const moviesRoutes: Routes = [
   {
     path: 'now-showing',
-    loadComponent: () => import('./pages/now-showing/now-showing').then((m) => m.NowShowing),
+    loadComponent: () =>
+      import('./pages/now-showing/now-showing.component').then((m) => m.NowShowing),
   },
   {
     path: 'upcoming',
-    loadComponent: () => import('./pages/upcoming/upcoming').then((m) => m.Upcoming),
+    loadComponent: () => import('./pages/upcoming/upcoming.component').then((m) => m.Upcoming),
   },
   {
     path: 'details/:id',
-    loadComponent: () => import('./pages/movie-details/movie-details').then((m) => m.MovieDetails),
+    loadComponent: () =>
+      import('./pages/movie-details/movie-details.component').then((m) => m.MovieDetails),
   },
 ];
--- Front/movie-app/src/app/layouts/main-layout/main-layout.ts
+++ Front/movie-app/src/app/layouts/main-layout/main-layout.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Navbar } from '../../components/navbar/navbar';
+import { Navbar } from '../../shared/components/navbar/navbar';
 import { Footer } from '../../components/footer/footer';
 
 @Component({
